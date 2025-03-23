d0334b410967c328c655eda245a6e880cd8d4a1f
tsurkanOdessa <set.tsurkan@gmail.com>
2025-03-23 10:32:58 +0100

    Python Advanced: Домашнее задание 4

--- app.py
+++ app.py
@@ -4,35 +4,28 @@ from Product import Product
 
 Base.metadata.create_all(engine)
 
-parent_category = Category(name="Electronics", description="Devices and gadgets")
-session.add(parent_category)
-session.commit()
+category_electronics = Category(name="Электроника", description="Гаджеты и устройства.")
+category_books = Category(name="Книги", description="Печатные книги и электронные книги.")
+category_clothing = Category(name="Одежда", description="Одежда для мужчин и женщин.")
 
-subcategory_s = Category(name="Smartphones", description="Mobile devices", parent_id=parent_category.id)
-session.add(subcategory_s)
-session.commit()
-
-subcategory_l = Category(name="Laptops", description="Laptop devices", parent_id=parent_category.id)
-session.add(subcategory_l)
-session.commit()
-
-new_product = Product(name="IPhon 16", price=999.99, in_stock=True, category_id=subcategory_s.id)
-session.add(new_product)
-session.commit()
-
-new_product = Product(name="Lenovo B71", price=599.99, in_stock=True, category_id=subcategory_l.id)
-session.add(new_product)
-session.commit()
-
-new_product = Product(name="ASUS N99", price=799.99, in_stock=True, category_id=subcategory_l.id)
-session.add(new_product)
+session.add_all([category_electronics, category_books, category_clothing])
 session.commit()
 
 categories = session.query(Category).all()
-products = session.query(Product).all()
 
 for category in categories:
-    print(f"Category: {category.name}, Parent: {category.parent_id}, Description: {category.description}")
+    parent_id= category.parent_id if category.parent_id else "None"
+    print(f"Category: {category.name}, Description: {category.description}, Parent: {parent_id}")
+
+products = session.query(Product).all()
+product_smartphone = Product(name="Смартфон", price=299.99, in_stock=True, category_id=category_electronics.id)
+product_laptop = Product(name="Ноутбук", price=499.99, in_stock=True, category_id=category_electronics.id)
+product_book = Product(name="Научно-фантастический роман", price=15.99, in_stock=True, category_id=category_books.id)
+product_jeans = Product(name="Джинсы", price=40.50, in_stock=True, category_id=category_clothing.id)
+product_tshirt = Product(name="Футболка", price=20.00, in_stock=True, category_id=category_clothing.id)
+
+session.add_all([product_smartphone, product_laptop, product_book, product_jeans, product_tshirt])
+session.commit()
 
 for product in products:
-    print(f"Product: {product.name}, Price: {product.price}, In Stock: {product.in_stock}, Category ID: {product.category_id}")
+    print(f"Product: {product.name}, Price: {product.price}, In Stock: {product.in_stock}, Category: {product.category.name}")
